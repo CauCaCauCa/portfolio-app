b34d0acd7bf1e54e1c0350bb8b9fe94d3f6f7c04
do tien <tiennt1242@gmail.com>
2023-07-05 16:21:05 +0700

    clear

--- src/components/_button/ButtonStyle.tsx
+++ src/components/_button/ButtonStyle.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import { Button } from '@mui/material'
 import './ButtonStyle.scss'
@@ -7,13 +8,14 @@ type Props = {
   style?: any,
   bgroundColor?: string,
   color?: string,
+  onClick?: () => void,
 }
 
-export default function ButtonStyle({ option, value, style, bgroundColor, color }: Props) {
+export default function ButtonStyle({ option, value, style, bgroundColor, color, onClick }: Props) {
   return (
     <>
       {option === '1' &&
-        <div id='btn-opt-1' style={style}>
+        <div id='btn-opt-1' style={style} onClick={onClick}>
           <Button className='btn' variant="contained" style={{ backgroundColor: 'black', position: 'absolute' }}>{value}</Button>
           <Button className='btn btn-hover' variant="contained" style={{ backgroundColor: bgroundColor, color: color }}>{value}</Button>
         </div>
